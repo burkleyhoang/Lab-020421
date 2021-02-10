7ac95a6e149e5226673828ae70dbd03d88722f9f
Burkley Hoang <67705100+burkleyhoang@users.noreply.github.com>
2021-02-10 09:29:40 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+/*
+    App: First Javascript Application
+    Author: Burkley Hoang
+    Date: 02/04/2021
+*/    
+
+    // Allow the user to enter their guess
+        let num = prompt('Guess a number between 1 and 10');
+    // Check to see if the number is a winner
+        if (num == 5) {
+            //If it is, they got it right
+            alert('You guessed the right number!');
+        } else {
+            //If it isn't, they got it wrong
+            alert('You guessed the wrong number');
+        }
